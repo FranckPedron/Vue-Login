c1efdc2c8897f4665aa12fc6a5db117fb77980ca
FranckPedron <franckpedrondev@gmail.com>
2022-11-11 19:44:06 +0100

    Build - Add password control

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <LoginView />
+</template>
+
+<script setup>
+import LoginView from './components/LoginView'
+
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/LoginView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text" id="name" placeholder="Utilisateur"/>
+    <input type="text" id="pass" placeholder="Mot de passe" @input="testLogin" v-model="password"/>
+    <p id="message">{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+let message = ref('');
+let password = ref('');
+
+function testLogin() {
+  if (password.value.match(/[0-9]/g) &&
+      password.value.match(/[a-z]/g) &&
+      password.value.match(/[A-Z]/g) &&
+      password.value.length >= 8
+  ) {
+    message.value = "Mot de passe correct";
+    document.getElementById('message').style.color = 'green';
+  } else {
+    message.value = "Mot de passe incorrect";
+    document.getElementById('message').style.color = 'red';
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
